6773fdb645c2bd2762fef1f72c00a17d0f56fe53
herotran <herotran210@gmail.com>
2020-06-13 15:02:51 +0700

    update package

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -6,7 +6,7 @@ import './home.scss';
 const Home = (props) => {
   return (
     <div className="wrapper-home">
-      <h1>Welcome you !</h1>
+      <h1>Welcome </h1>
       <p>{i18n.t('common.about')}</p>
       <Grid container spacing={0}>
         <Grid item xs={12} sm={6}>
